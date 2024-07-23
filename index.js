c307629fe77d523562c4284f16074a22188450c1
Asma Akbar <masoomlarki297@gmail.com>
2024-07-23 21:29:29 +0500

    push my new stlye calculator

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
-    { message: "select one of the operators to perform operation",
+    { message: chalk.yellowBright("\n Enter first number"), type: "number", name: "firstNumber" },
+    { message: chalk.blue(" \n Enter second number"), type: "number", name: "secondNumber" },
+    { message: chalk.whiteBright("select one of the operators to perform operation"),
         type: "list",
         name: "operator",
         choices: ["Addition", "Subtraction", "Multiplication", "Division"], },
 ]);
 if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
+    console.log(chalk.yellowBright(answer.firstNumber + answer.secondNumber));
 }
 else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
+    console.log(chalk.blue(answer.firstNumber - answer.secondNumber));
 }
 else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
+    console.log(chalk.yellowBright(answer.firstNumber * answer.secondNumber));
 }
 else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber);
+    console.log(chalk.blue(answer.firstNumber / answer.secondNumber));
 }
 else {
-    console.log("please select corret number");
+    console.log(chalk.red("please select corret number"));
 }
 console.log("THE END");
+console.log("_".repeat(60));
